5709b0ea85faa69688451622bcbca32cae1bc7f6
ujjwal kumar <ujjwalk2007@gmail.com>
2026-04-07 22:37:10 +0530

    fix pdf import

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import multer from 'multer';
 import fs from 'fs';
-import pdfParse from 'pdf-parse';
+import pdfParse from 'pdf-parse/lib/index.js';
 import mammoth from 'mammoth';
 import sharp from 'sharp';
 
